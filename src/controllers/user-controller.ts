a300d423cc04cd6d131514feb0ae6cdc29ddbd37
diegodevtech <diegodevtechnology@gmail.com>
2026-05-08 14:20:20 -0400

    feature: zod validation
    -tests creating an user to see validation on duty

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,7 +1,17 @@
+import { UserRole } from '@/generated/prisma/enums';
 import { Request, Response } from 'express';
+import { z } from 'zod';
 
 export class UserController {
   async create(request: Request, response: Response) {
-    response.status(201).json({ message: 'User created successfully' });
+    const createUserSchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.email({ error: "Invalid email address" }).toLowerCase(),
+      password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+      role: z.enum([UserRole.EMPLOYEE, UserRole.MANAGER], { message: "Role must be either 'EMPLOYEE' or 'MANAGER'" }).default(UserRole.EMPLOYEE),
+    });
+
+    const { name, email, password, role } = createUserSchema.parse(request.body);
+    response.status(201).json({ name, password, email, role });
   }
 }
--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,6 +1,7 @@
 import { AppError } from "@/utils/AppError";
 import { ErrorRequestHandler } from "express";
 import { ZodError } from "zod";
+import { z } from "zod";
 
 export const errorHandlingMiddleware: ErrorRequestHandler = (err, req, res, next) => {
   if (err instanceof AppError) {
@@ -11,10 +12,9 @@ export const errorHandlingMiddleware: ErrorRequestHandler = (err, req, res, next
 
   if (err instanceof ZodError) {
     return res.status(400).json({
-      message: "Validation error",
-      issues: err.format,
+      message: z.treeifyError(err),
     });
-  }
+}
 
   return res.status(500).json({
     message: err.message ||"Internal Server Error",
