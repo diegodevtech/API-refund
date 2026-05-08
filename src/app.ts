3cc3659582cca28ff9fb24c7b373e8e92d757696
diegodevtech <diegodevtechnology@gmail.com>
2026-05-08 12:35:24 -0400

    config: cors
    -adds cors to the project

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import cors from "cors";
 
-export const app = express();
+const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => { res.send("Hello World!"); });
+
+export { app };
