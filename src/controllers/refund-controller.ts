ffecb0e00275b616fa0266a8a360b1fea3ce0626
diegodevtech <diegodevtechnology@gmail.com>
2026-05-08 17:25:16 -0400

    feature: creating refund
    -finishes logic for creating refund

--- src/controllers/refund-controller.ts
+++ src/controllers/refund-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
-import { Category as Categories } from '@/generated/prisma/enums';
+import { Category as Categories, UserRole } from '@/generated/prisma/enums';
+import { AppError } from '@/utils/AppError';
 import { z } from 'zod';
+import { prisma } from '@/database/prisma';
 
 export class RefundController {
   async create(req: Request, res: Response) {
@@ -18,6 +20,24 @@ export class RefundController {
     });
 
     const { name, category, amount, filename } = bodySchema.parse(req.body);
-    return res.json({ message: 'Ok' });
+
+    if(!req.user || !req.user?.id) {
+      throw new AppError('User not found', 404);
+    }
+
+    if(!req.user?.role || req.user?.role !== UserRole.EMPLOYEE) {
+      throw new AppError('Unauthorized', 403);
+    }
+
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        category,
+        amount,
+        filename,
+        userId: req.user.id
+      }
+    })
+    res.status(201).json(refund);
   }
 }
