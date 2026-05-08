bfb8cd52c3af2824e6088c2431ed176f4dabe6f0
diegodevtech <diegodevtechnology@gmail.com>
2026-05-08 17:13:42 -0400

    feature: refund request
    -adds initial logic for requesting a refund

--- src/controllers/refund-controller.ts
+++ src/controllers/refund-controller.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from 'express';
+import { Category as Categories } from '@/generated/prisma/enums';
+import { z } from 'zod';
 
 export class RefundController {
   async create(req: Request, res: Response) {
-    return res.status(201).json({ message: 'Refund created successfully' });
+    const bodySchema = z.object({
+      name: z.string().trim().min(2).max(255),
+      category: z.enum([
+        Categories.FOOD,
+        Categories.SERVICES,
+        Categories.TRANSPORT,
+        Categories.ACCOMMODATION,
+        Categories.OTHER
+      ], { message: 'Invalid category' }),
+      amount: z.number().positive({ message: 'Amount must be a positive number' }),
+      filename: z.string().trim().min(1, { message: 'Filename is required' }),
+    });
+
+    const { name, category, amount, filename } = bodySchema.parse(req.body);
+    return res.json({ message: 'Ok' });
   }
 }
